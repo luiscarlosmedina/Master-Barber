6d418a73879e3a3bfc19ac0ec4b2232e4ac9aabc
DavidV4iss <163668311+DavidV4iss@users.noreply.github.com>
2024-05-29 20:02:11 -0500

    SI
    DI

--- BASE DE DATOS/master_barber.sql
+++ BASE DE DATOS/master_barber.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2024 a las 00:36:16
+-- Tiempo de generación: 30-05-2024 a las 02:40:48
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.1.17
 
